c699eaefd4b6a014b3ddc7ccd88add56e603dcd5
Saesha Senger <saesha.senger@gmail.com>
2020-06-04 13:53:01 -0400

    changes omdb api to https, adds video functionality for NASA api

--- GroupProject/index.js
+++ GroupProject/index.js
@@ -16,11 +16,31 @@ function getRandomIndex(max) {
 function getSpace() {
   $.get('https://api.nasa.gov/planetary/apod?api_key=QwweL5wp7rhdhr0ZBx0qddvzMvy2l14gk6EfryqL')
     .then(function (data2) {
+      const vid = document.getElementById('space-video');
       const img = document.getElementsByTagName('img')[3];
       console.log('data =', data2)
-      img.setAttribute('src', data2.url)
+      if (data2.media_type === "video") {
+        vid.setAttribute('src', data2.url);
+        vid.style.display = "inline"
+        img.style.display = "none"
+      }
+      else {
+        img.setAttribute('src', data2.url);
+      }
     });
 };
+
+/* 
+
+{date: "2020-06-03", explanation: "very time Venus passes the Earth, it shows the sam…ll humanity first discover extraterrestrial life?", media_type: "video", service_version: "v1", title: "The Dance of Venus and Earth", …}
+date: "2020-06-03"
+explanation: "very time Venus passes the Earth, it shows the same face.  This remarkable fact has been known for only about 50 years, ever since radio telescopes have been able to peer beneath Venus' thick clouds and track its slowly rotating surface.  This inferior conjunction -- when Venus and Earth are the closest -- occurs today.  The featured animation shows the positions of the Sun, Venus and Earth between 2010-2023 based on NASA-downloaded data, while a mock yellow 'arm' has been fixed to the ground on Venus to indicate rotation.  The reason for this unusual 1.6-year resonance is the gravitational influence that Earth has on Venus, which surprisingly dominates the Sun's tidal effect.  If Venus could be seen through the Sun's glare today, it would show just a very slight sliver of a crescent. Although previously visible in the evening sky, starting tomorrow, Venus will appear in the morning sky -- on the other side of the Sun as viewed from Earth.   Experts Debate: How will humanity first discover extraterrestrial life?"
+media_type: "video"
+service_version: "v1"
+title: "The Dance of Venus and Earth"
+url: "https://www.youtube.com/embed/Cd5a5KdPxQc?rel=0"
+__proto__: Object
+*/
 
 ///Sets cat image api attached to random quote card
 function getCats() {
